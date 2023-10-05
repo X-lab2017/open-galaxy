2509f116d5e30e77a3f6846652d94d3c72262539
Lam Tang <tangyenan@gmail.com>
2023-10-05 15:35:22 +0800

    fix: should display nothing when data is not found

--- src/galaxy/nodeDetails/TwoNetworks/index.jsx
+++ src/galaxy/nodeDetails/TwoNetworks/index.jsx
@@ -17,48 +17,56 @@ export const TwoNetworks = ({ nodeName }) => {
   useEffect(() => {
     const fetchData = async () => {
       const url1 = `https://oss.x-lab.info/open_digger/github/${nodeName}/repo_network.json`;
-      const data1 = await request(url1, {
-        responseType: "json",
-      });
-      if (data1.hasOwnProperty("error")) {
-        throw `"${nodeName}": ${data1.error}`;
+      try {
+        const data1 = await request(url1, {
+          responseType: "json",
+        });
+        setProjectNetworkData(data1);
+
+      } catch (error) {
+        setProjectNetworkData(null);
       }
-      setProjectNetworkData(data1);
 
       const url2 = `https://oss.x-lab.info/open_digger/github/${nodeName}/developer_network.json`;
-      const data2 = await request(url2, {
-        responseType: "json",
-      });
-      if (data2.hasOwnProperty("error")) {
-        throw `"${nodeName}": ${data2.error}`;
+      try {
+        const data2 = await request(url2, {
+          responseType: "json",
+        });
+        setContributorNetworkData(data2);
+
+      } catch (error) {
+        setContributorNetworkData(null);
       }
-      setContributorNetworkData(data2);
     }
     fetchData();
   }, [nodeName]);
-
-  if (!projectNetworkData || !contributorNetworkData) return null;
 
   return (
     <div className="grid-row two-networks">
       {/* project network */}
       <div className="project-network">
         <h4 className="text-center">{intl.get("PROJECT_CORRELATION_NETWORK")}</h4>
-        <Graph
-          data={projectNetworkData}
-          style={graphStyle}
-          focusedNodeID={nodeName}
-        />
+        {
+          projectNetworkData &&
+          <Graph
+            data={projectNetworkData}
+            style={graphStyle}
+            focusedNodeID={nodeName}
+          />
+        }
       </div>
       {/* divider */}
       <div className="vertical-divider" />
       {/* contributor network */}
       <div className="contributor-network">
         <h4 className="text-center">{intl.get("CONTRIBUTOR_CORRELATION_NETWORK")}</h4>
-        <Graph
-          data={contributorNetworkData}
-          style={graphStyle}
-        />
+        {
+          contributorNetworkData &&
+          <Graph
+            data={contributorNetworkData}
+            style={graphStyle}
+          />
+        }
       </div>
     </div>
   );
